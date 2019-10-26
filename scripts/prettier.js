0680449dca0df30328209572e52a949d98c9a943
Fea-Sin <1210992614@qq.com>
2019-10-26 16:28:23 +0800

    add style

--- scripts/prettier.js
+++ scripts/prettier.js
@@ -33,19 +33,18 @@ if (program.pre) {
     '**/components/style/color/**',
     '**/dist/**',
     '_site/**',
-  ]
+  ];
 
   // get all ts, js, less files
   ['**/*.ts*', '**/*.js*', '**/*.less', '**/*.md', '**/*.html'].forEach(pattern => {
     const matchFiles = glob.sync(pattern, {
-      ignore: ignoreFiles
-    })
-    files = files.concat(matchFiles)
-  })
+      ignore: ignoreFiles,
+    });
+    files = files.concat(matchFiles);
+  });
 }
-
 if (!files.length) {
-  return
+  return;
 }
 
 let didError = false;
@@ -54,36 +53,36 @@ let didWarn = false;
 files.forEach(file => {
   const options = prettier.resolveConfig.sync(file, {
     config: prettierConfigPath,
-  })
-  const fileInfo = prettier.getFileInfo.sync(file)
+  });
+  const fileInfo = prettier.getFileInfo.sync(file);
   if (fileInfo.ignored) {
-    return
+    return;
   }
   try {
-    const input = fs.readFileSync(file, 'utf8')
+    const input = fs.readFileSync(file, 'utf8');
     const withParserOptions = {
       ...options,
       parser: fileInfo.inferredParser,
-    }
+    };
     if (program.lint) {
-      const isPrettier = prettier.check(input, withParserOptions)
+      const isPrettier = prettier.check(input, withParserOptions);
       if (!isPrettier) {
         // eslint-disable-next-line no-console
-        console.log(chalk.red(`${file} is no prettier, please use npm run prettier and git add !`))
-        didWarn = true
+        console.log(chalk.red(`${file} is no prettier, please use npm run prettier and git add !`));
+        didWarn = true;
       }
     } else {
-      const output = prettier.format(input, withParserOptions)
+      const output = prettier.format(input, withParserOptions);
       if (output !== input) {
-        fs.writeFileSync(file, output, 'utf8')
+        fs.writeFileSync(file, output, 'utf8');
         // eslint-disable-next-line no-console
-        console.log(chalk.blue(`${file} is prettier`))
+        console.log(chalk.blue(`${file} is prettier`));
       }
     }
   } catch (e) {
-    didError = true
+    didError = true;
   }
-})
+});
 
 if (didWarn || didError) {
   process.exit(1);
