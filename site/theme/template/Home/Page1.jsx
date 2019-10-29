17f6e26e55b2ad0d658d49b7edab62a671f031e6
Fea-Sin <1210992614@qq.com>
2019-10-29 16:41:14 +0800

    add template home

---
+++ site/theme/template/Home/Page1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class Page1 extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+        Page1
+      </div>
+    )
+  }
+}
---
+++ site/theme/template/Home/Page2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class Page1 extends React.PureComponent {
+
+  render() {
+    return (
+      <div>
+        Page2
+      </div>
+    )
+  }
+}
