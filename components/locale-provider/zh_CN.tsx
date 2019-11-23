fdf0a53cd4b324fb0ac4e3c2a06ff1e9fcbb334f
Fea-Sin <1210992614@qq.com>
2019-11-23 15:52:25 +0800

    add localeProvider

---
+++ components/locale-provider/zh_CN.tsx
@@ -0,0 +1,12 @@
+import Pagination from 'rc-pagination/lib/locale/zh_CN';
+
+export default {
+  locale: 'zh-cn',
+  Pagination,
+  global: {
+    placeholder: '请选择',
+  },
+  Icon: {
+    icon: '图标',
+  },
+};
